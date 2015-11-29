a5f0722f7474db0227dc1c71fa6b51a34e3c6de9
seafeawea <452576504@qq.com>
2015-11-29 14:58:56 +0800

    Initial Commit

---
+++ logger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  logger
+//
+//  Created by XZ on 15/11/29.
+//  Copyright © 2015年 XZ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
